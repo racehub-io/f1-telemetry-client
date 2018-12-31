0b0c01f8cf4b0b620707707c29a335ad04acc7e9
Jonathan Bursztyn <jonathan@bursztyn.io>
2018-12-31 18:58:31 -0300

    Add driver names, add support for floating number arrays

--- src/parsers/CarMotionData.js
+++ src/parsers/CarMotionData.js
@@ -1,28 +1,28 @@
 import F1Parser from './F1Parser';
 
 /**
-   * struct CarMotionData
-   * {
-   *   float         m_worldPositionX;           // World space X position
-   *   float         m_worldPositionY;           // World space Y position
-   *   float         m_worldPositionZ;           // World space Z position
-   *   float         m_worldVelocityX;           // Velocity in world space X
-   *   float         m_worldVelocityY;           // Velocity in world space Y
-   *   float         m_worldVelocityZ;           // Velocity in world space Z
-   *   int16         m_worldForwardDirX;         // World space forward X direction (normalised)
-   *   int16         m_worldForwardDirY;         // World space forward Y direction (normalised)
-   *   int16         m_worldForwardDirZ;         // World space forward Z direction (normalised)
-   *   int16         m_worldRightDirX;           // World space right X direction (normalised)
-   *   int16         m_worldRightDirY;           // World space right Y direction (normalised)
-   *   int16         m_worldRightDirZ;           // World space right Z direction (normalised)
-   *   float         m_gForceLateral;            // Lateral G-Force component
-   *   float         m_gForceLongitudinal;       // Longitudinal G-Force component
-   *   float         m_gForceVertical;           // Vertical G-Force component
-   *   float         m_yaw;                      // Yaw angle in radians
-   *   float         m_pitch;                    // Pitch angle in radians
-   *   float         m_roll;                     // Roll angle in radians
-   * };
-   */
+ * struct CarMotionData
+ * {
+ *   float         m_worldPositionX;           // World space X position
+ *   float         m_worldPositionY;           // World space Y position
+ *   float         m_worldPositionZ;           // World space Z position
+ *   float         m_worldVelocityX;           // Velocity in world space X
+ *   float         m_worldVelocityY;           // Velocity in world space Y
+ *   float         m_worldVelocityZ;           // Velocity in world space Z
+ *   int16         m_worldForwardDirX;         // World space forward X direction (normalised)
+ *   int16         m_worldForwardDirY;         // World space forward Y direction (normalised)
+ *   int16         m_worldForwardDirZ;         // World space forward Z direction (normalised)
+ *   int16         m_worldRightDirX;           // World space right X direction (normalised)
+ *   int16         m_worldRightDirY;           // World space right Y direction (normalised)
+ *   int16         m_worldRightDirZ;           // World space right Z direction (normalised)
+ *   float         m_gForceLateral;            // Lateral G-Force component
+ *   float         m_gForceLongitudinal;       // Longitudinal G-Force component
+ *   float         m_gForceVertical;           // Vertical G-Force component
+ *   float         m_yaw;                      // Yaw angle in radians
+ *   float         m_pitch;                    // Pitch angle in radians
+ *   float         m_roll;                     // Roll angle in radians
+ * };
+ */
 export default class CarMotionData extends F1Parser {
   constructor() {
     super();
--- src/parsers/PacketMotionData.js
+++ src/parsers/PacketMotionData.js
@@ -1,5 +1,34 @@
 import F1Parser from './F1Parser';
 import CarMotionData from './CarMotionData';
+import {
+  Parser
+} from 'binary-parser';
+
+/*
+struct PacketMotionData
+{
+    PacketHeader    m_header;               // Header
+
+    CarMotionData   m_carMotionData[20];    // Data for all cars on track
+
+    // Extra player car ONLY data
+    float         m_suspensionPosition[4];       // Note: All wheel arrays have the following order:
+    float         m_suspensionVelocity[4];       // RL, RR, FL, FR
+    float         m_suspensionAcceleration[4];   // RL, RR, FL, FR
+    float         m_wheelSpeed[4];               // Speed of each wheel
+    float         m_wheelSlip[4];                // Slip ratio for each wheel
+    float         m_localVelocityX;              // Velocity in local space
+    float         m_localVelocityY;              // Velocity in local space
+    float         m_localVelocityZ;              // Velocity in local space
+    float         m_angularVelocityX;            // Angular velocity x-component
+    float         m_angularVelocityY;            // Angular velocity y-component
+    float         m_angularVelocityZ;            // Angular velocity z-component
+    float         m_angularAccelerationX;        // Angular velocity x-component
+    float         m_angularAccelerationY;        // Angular velocity y-component
+    float         m_angularAccelerationZ;        // Angular velocity z-component
+    float         m_frontWheelsAngle;            // Current front wheels angle in radians
+};
+*/
 
 export default class PacketMotionData extends F1Parser {
   constructor(buffer) {
@@ -10,29 +39,26 @@ export default class PacketMotionData extends F1Parser {
         length: 20,
         type: new CarMotionData(),
       })
-      /*
       .array('m_suspensionPosition', {
         length: 4,
-        type: 'float'
-      })       // Note: All wheel arrays have the following order:
+        type: new Parser().floatle()
+      })
       .array('m_suspensionVelocity', {
         length: 4,
-        type: 'float'
-      })       // RL, RR, FL, FR
+        type: new Parser().floatle()
+      })
       .array('m_suspensionAcceleration', {
         length: 4,
-        type: 'float'
-      })   // RL, RR, FL, FR
+        type: new Parser().floatle()
+      })
       .array('m_wheelSpeed', {
         length: 4,
-        type: 'float'
-      })               // Speed of each wheel
+        type: new Parser().floatle()
+      })
       .array('m_wheelSlip', {
         length: 4,
-        type: 'float'
-      })                // Slip ratio for each wheel
-      */
-      .skip(16 * 5)
+        type: new Parser().floatle()
+      })
       .float('m_localVelocityX') // Velocity in local space
       .float('m_localVelocityY') // Velocity in local space
       .float('m_localVelocityZ') // Velocity in local space
