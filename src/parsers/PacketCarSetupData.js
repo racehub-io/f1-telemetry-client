2dd42d81d155fd371a15fbc7840b3594c6b076fb
Jonathan Bursztyn <jonathan@bursztyn.io>
2019-01-05 16:50:43 -0300

    Implemented telemetry packet

---
+++ src/parsers/PacketCarSetupData.js
@@ -0,0 +1,25 @@
+import F1Parser from './F1Parser';
+import CarSetupData from './CarSetupData';
+
+/*
+struct PacketCarSetupData
+{
+  PacketHeader    m_header;            // Header
+  CarSetupData    m_carSetups[20];
+};
+*/
+
+export default class PacketCarSetupData extends F1Parser {
+  constructor(buffer) {
+    super();
+    this.endianess('little')
+      // skips the header
+      .skip(21)
+      .array('m_carSetups', {
+        length: 20,
+        type: new CarSetupData()
+      })
+
+    this.data = this.fromBuffer(buffer);
+  }
+}
---
+++ src/parsers/PacketCarTelemetryData.js
@@ -0,0 +1,28 @@
+import F1Parser from "./F1Parser";
+import CarTelemetryData from "./CarTelemetryData";
+
+/*
+struct PacketCarTelemetryData
+{
+  PacketHeader        m_header;                // Header
+  CarTelemetryData    m_carTelemetryData[20];
+  uint32              m_buttonStatus;          // Bit flags specifying which buttons are being
+                                               // pressed currently - see appendices
+};
+*/
+
+export default class PacketCarTelemetryData extends F1Parser {
+  constructor(buffer) {
+    super();
+    this.endianess("little")
+      // skips the header
+      .skip(21)
+      .array("m_carTelemetryData", {
+        length: 20,
+        type: new CarTelemetryData()
+      })
+      .uint32le('m_buttonStatus')
+
+    this.data = this.fromBuffer(buffer);
+  }
+}
