41c9e83d00e8c19d350ae70b4cfcf571ca68ba73
Jonathan Bursztyn <jonathan@bursztyn.io>
2019-01-12 22:30:08 -0300

    LapData now receives LapData from all racers

--- src/parsers/packets/PacketLapData.js
+++ src/parsers/packets/PacketLapData.js
@@ -1,30 +1,11 @@
 import F1Parser from '../F1Parser';
+import LapData from './LapData'
 
 /*
-struct LapData
+struct PacketLapData
 {
-    float       m_lastLapTime;           // Last lap time in seconds
-    float       m_currentLapTime;        // Current time around the lap in seconds
-    float       m_bestLapTime;           // Best lap time of the session in seconds
-    float       m_sector1Time;           // Sector 1 time in seconds
-    float       m_sector2Time;           // Sector 2 time in seconds
-    float       m_lapDistance;           // Distance vehicle is around current lap in metres – could
-                                         // be negative if line hasn’t been crossed yet
-    float       m_totalDistance;         // Total distance travelled in session in metres – could
-                                         // be negative if line hasn’t been crossed yet
-    float       m_safetyCarDelta;        // Delta in seconds for safety car
-    uint8       m_carPosition;           // Car race position
-    uint8       m_currentLapNum;         // Current lap number
-    uint8       m_pitStatus;             // 0 = none, 1 = pitting, 2 = in pit area
-    uint8       m_sector;                // 0 = sector1, 1 = sector2, 2 = sector3
-    uint8       m_currentLapInvalid;     // Current lap invalid - 0 = valid, 1 = invalid
-    uint8       m_penalties;             // Accumulated time penalties in seconds to be added
-    uint8       m_gridPosition;          // Grid position the vehicle started the race in
-    uint8       m_driverStatus;          // Status of driver - 0 = in garage, 1 = flying lap
-                                         // 2 = in lap, 3 = out lap, 4 = on track
-    uint8       m_resultStatus;          // Result status - 0 = invalid, 1 = inactive, 2 = active
-                                         // 3 = finished, 4 = disqualified, 5 = not classified
-                                         // 6 = retired
+    PacketHeader    m_header;              // Header
+    LapData         m_lapData[20];         // Lap data for all cars on track
 };
 */
 
@@ -32,29 +13,11 @@ export default class PacketLapData extends F1Parser {
   constructor(buffer) {
     super();
     this.endianess('little')
-      // skips the header
-      //.buffer('m_header', {
-      //  length: 21,
-      //  formatter: new PacketHeader()
-      //})
       .skip(21)
-      .floatle('m_lastLapTime') // Last lap time in seconds
-      .floatle('m_currentLapTime') // Current time around the lap in seconds
-      .floatle('m_bestLapTime') // Best lap time of the session in seconds
-      .floatle('m_sector1Time') // Sector 1 time in seconds
-      .floatle('m_sector2Time') // Sector 2 time in seconds
-      .floatle('m_lapDistance') // Distance vehicle is around current lap in metres – could be negative if line hasn’t been crossed yet
-      .floatle('m_totalDistance') // Total distance travelled in session in metres – could be negative if line hasn’t been crossed yet
-      .floatle('m_safetyCarDelta') // Delta in seconds for safety car
-      .uint8("m_carPosition") // Car race position
-      .uint8("m_currentLapNum") // Current lap number
-      .uint8("m_pitStatus") // 0 = none, 1 = pitting, 2 = in pit area
-      .uint8("m_sector") // 0 = sector1, 1 = sector2, 2 = sector3
-      .uint8("m_currentLapInvalid") // Current lap invalid - 0 = valid, 1 = invalid
-      .uint8("m_penalties") // Accumulated time penalties in seconds to be added
-      .uint8("m_gridPosition") // Grid position the vehicle started the race in
-      .uint8("m_driverStatus") // Status of driver - 0 = in garage, 1 = flying lap 2 = in lap, 3 = out lap, 4 = on track
-      .uint8("m_resultStatus") // Result status - 0 = invalid, 1 = inactive, 2 = active 3 = finished, 4 = disqualified, 5 = not classified 6 = retired
+      .array('m_lapData', {
+        length: 20,
+        type: new LapData(),
+      })
 
     this.data = this.fromBuffer(buffer);
   }
