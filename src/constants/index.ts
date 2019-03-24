e534a31b53b74370fceaedb13c16afbcfc735831
Jonathan Bursztyn <jonathan@bursztyn.io>
2019-03-24 02:25:54 -0300

    Fix linting

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,8 @@
-import {
-  DRIVERS,
-} from './drivers';
-import {
-  TRACKS,
-} from './tracks';
-import {
-  TEAMS,
-} from './teams';
-import {
-  PACKETS,
-  Packets,
-} from './packets';
+import { DRIVERS } from './drivers';
+import { TRACKS } from './tracks';
+import { TEAMS } from './teams';
+import { PACKETS, Packets } from './packets';
 
 export {
-  Packets,
-  PACKETS,
-  TRACKS,
-  DRIVERS,
-  TEAMS,
+  Packets, PACKETS, TRACKS, DRIVERS, TEAMS,
 };
--- src/constants/weather.ts
+++ src/constants/weather.ts
@@ -1,10 +1,10 @@
 const WEATHER = [
-  "CLEAR",
-  "LIGHT_CLOUD",
-  "OVERCAST",
-  "LIGHT_RAIN",
-  "HEAVY_RAIN",
-  "STORM"
+  'CLEAR',
+  'LIGHT_CLOUD',
+  'OVERCAST',
+  'LIGHT_RAIN',
+  'HEAVY_RAIN',
+  'STORM',
 ];
 
 export default WEATHER;
