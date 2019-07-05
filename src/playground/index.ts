940dc289d3f989ba3e35fb90ea1d18491f4045f1
Jonathan Bursztyn <jobur93@gmail.com>
2019-07-04 23:04:18 -0300

    Restore playground

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -4,18 +4,24 @@ const {PACKETS} = constants;
 
 const client = new F1TelemetryClient({port: 20777});
 
-client.on(PACKETS.session, e => {});
-client.on(PACKETS.participants, e => {});
-client.on(PACKETS.carTelemetry, e => {});
-client.on(PACKETS.motion, e => {});
-client.on(PACKETS.lapData, e => {});
-client.on(PACKETS.event, e => {});
-client.on(PACKETS.carSetups, e => {});
-client.on(PACKETS.carStatus, e => {});
+client.on(PACKETS.session, console.log);
+client.on(PACKETS.motion, console.log);
+client.on(PACKETS.lapData, console.log);
+client.on(PACKETS.event, console.log);
+client.on(PACKETS.participants, console.log);
+client.on(PACKETS.carSetups, console.log);
+client.on(PACKETS.carTelemetry, console.log);
+client.on(PACKETS.carStatus, console.log);
 
 client.start();
 
 // stops the client
-[].forEach(eventType => {
+[`exit`,
+ `SIGINT`,
+ `SIGUSR1`,
+ `SIGUSR2`,
+ `uncaughtException`,
+ `SIGTERM`,
+].forEach(eventType => {
   (process as NodeJS.EventEmitter).on(eventType, () => client.stop());
 });
